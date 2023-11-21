feaa633869f4686b10da1bce1d2b5021e012d540
Ericx44 <eric.deguzman0012@gmail.com>
2023-11-21 15:48:44 +0800

    Monthly Sales is Needed to Finish this

--- export.php
+++ export.php
@@ -68,7 +68,7 @@ foreach ($categories as $category) {
     $sheet->setTitle($category);
 
     // Set column headers
-    $header = ['Vendor'];
+    $header = [$vendor];
     $sheet->mergeCells('A1:H1');
     $style = $sheet->getStyle('A1');
     $alignment = $style->getAlignment();
@@ -120,96 +120,7 @@ foreach ($categories as $category) {
         $data = $stmt->fetchAll(PDO::FETCH_NUM);
 
         // Add data to the current sheet and assign random colors to each row
-        $row = $sheet->getHighestRow() + 1; // Start from the next available row
-        $currentMonth = null;
-        foreach ($data as $rowData) {
-            // If the month changes, add an empty row as a separator
-            if ($currentMonth !== null && $currentMonth !== $rowData[7]) { // Assuming the date column is the 8th column (index 7)
-                $row++;
-                $sheet->fromArray(['', '', '', '', '', '', '', ''], null, 'A' . $row);
-            }
-
-            $sheet->fromArray($rowData, null, 'A' . $row);
-
-            // Assign random color to the row
-            $color = sprintf('#%06X', mt_rand(0, 0xFFFFFF));
-            $sheet->getStyle('A' . $row . ':H' . $row)->applyFromArray([
-                'fill' => [
-                    'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
-                    'startColor' => ['rgb' => $color],
-                ],
-            ]);
-
-            $row++;
-            $currentMonth = $rowData[7];
-        }
-
-        // Add overall summary row at the bottom for the current vendor
-        $lastRow = $sheet->getHighestRow();
-        $summaryRow = ['Total', '', '', '', '', '=SUM(F2:F' . $lastRow . ')', '', ''];
-        $sheet->fromArray($summaryRow, null, 'A' . ($lastRow + 1));
-    }
-}
-
-foreach ($categories as $category) {
-    // Add a new sheet for each category
-    $sheet = $spreadsheet->createSheet();
-    $sheet->setTitle($category);
-
-    // Set column headers
-    $header = ['Vendor'];
-    $sheet->mergeCells('A1:H1');
-    $style = $sheet->getStyle('A1');
-    $alignment = $style->getAlignment();
-    $alignment->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
-    $sheet->fromArray($header, null, 'A1');
-    $headers = ['ID', 'Product', 'Quantity', 'Category', 'Price', 'Total', 'Remarks', 'Date'];
-    $sheet->fromArray($headers, null, 'A2');
-
-    // Fetch unique vendors for the current category
-    $vendorSql = "SELECT DISTINCT v.vendor_name
-                  FROM sales s
-                  LEFT JOIN vendor v ON v.id = s.vendor_id
-                  LEFT JOIN categories c ON c.id = s.category_id
-                  WHERE c.name = :category";
-    $vendorStmt = $pdo->prepare($vendorSql);
-    $vendorStmt->bindParam(':category', $category);
-    $vendorStmt->execute();
-    $vendors = $vendorStmt->fetchAll(PDO::FETCH_COLUMN);
-
-    // Loop through each vendor
-    foreach ($vendors as $vendor) {
-        // Fetch data for the current category and vendor from the database
-        $sql = "SELECT 
-            s.id AS ID, 
-            p.name as Product_Name, 
-            c.name as Category, 
-            s.qty AS Quantity, 
-            s.price as Price, 
-            s.price * s.qty as Total, 
-            s.remarks as Remarks, 
-            DATE_FORMAT(s.date, '%Y-%m') as month_year
-        FROM 
-            sales s
-        LEFT JOIN 
-            categories c ON c.id = s.category_id
-        LEFT JOIN 
-            products p ON p.id = s.product_id
-        LEFT JOIN 
-            vendor v ON v.id = s.vendor_id
-        WHERE c.name = :category AND v.vendor_name = :vendor
-        GROUP BY 
-            month_year, s.id, p.name, v.vendor_name, c.name, s.qty, s.price, s.remarks
-        ORDER BY 
-            month_year ASC, s.id ASC";
-        $stmt = $pdo->prepare($sql);
-        $stmt->bindParam(':category', $category);
-        $stmt->bindParam(':vendor', $vendor);
-        $stmt->execute();
-        $data = $stmt->fetchAll(PDO::FETCH_NUM);
-
-        // Add data to the current sheet and assign random colors to each row
-        $row = $sheet->getHighestRow() + 1; // Start from the next available row
+        $row = $sheet->getHighestRow() + 3; // Start from the next available row
         $currentMonth = null;
         foreach ($data as $rowData) {
             // If the month changes, add an empty row as a separator
--- phpexcel.php
+++ phpexcel.php
@@ -9,38 +9,114 @@ $username = "root";
 $password = "";
 $dbname = "inventory_system";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$query = "
-    SELECT s.id AS ID, p.name as Product_Name, v.vendor_name as Vendor, s.qty AS Quantity, s.price as Price, s.price*s.qty as Total, s.remarks as Remarks, s.date
-    FROM sales s
-    INNER JOIN products p ON p.id = s.product_id
-    INNER JOIN vendor v ON v.id = s.vendor_id
-    ORDER BY s.id ASC
-";
+$spreadsheet = new Spreadsheet;
 
-$result = $conn->query($query);
-$objPHPExcel = new Spreadsheet;
-$rowCount = 1;
-while ($row = $result->fetch_assoc()) {
-    $objPHPExcel->getActiveSheet()->setCellValue('A'.$rowCount, $row['Product_Name']);
-    $objPHPExcel->getActiveSheet()->setCellValue('B'.$rowCount, $row['Quantity']);
-    $objPHPExcel->getActiveSheet()->setCellValue('C'.$rowCount, $row['Price']);
-    $objPHPExcel->getActiveSheet()->setCellValue('D'.$rowCount, $row['Total']);
-    $objPHPExcel->getActiveSheet()->setCellValue('E'.$rowCount, $row['Vendor']);
-    $objPHPExcel->getActiveSheet()->setCellValue('F'.$rowCount, $row['Remarks']);
-    $objPHPExcel->getActiveSheet()->setCellValue('G'.$rowCount, $row['date']);
-    $rowCount++;
+$firstSheet = $spreadsheet->getActiveSheet();
+$firstSheet->setTitle('Overall Summary');
+
+// Set column headers for the first sheet
+$firstSheetHeaders = ['Category ID', 'Category Name', 'Total Sales'];
+$firstSheet->fromArray([$firstSheetHeaders], null, 'A1');
+
+// Fetch overall summary data for all categories
+$sql = "SELECT 
+    c.id AS CategoryID,
+    c.name AS CategoryName,
+    SUM(s.qty * s.price) AS TotalSales
+FROM 
+    sales s
+RIGHT JOIN 
+    categories c ON c.id = s.category_id
+GROUP BY 
+    c.id, c.name";
+
+// Use the connection variable you've already created
+$categoryResult = $conn->query($sql);
+$categorySummaries = $categoryResult->fetch_all(MYSQLI_ASSOC);
+
+// Add overall summary rows to the first sheet
+$row = 2;
+foreach ($categorySummaries as $categorySummary) {
+    $firstSheet->fromArray([$categorySummary['CategoryID'], $categorySummary['CategoryName'], $categorySummary['TotalSales']], null, 'A' . $row);
+    $row++;
 }
 
-$conn-> close();
+// Reset the pointer to the beginning of the result set
+$categoryResult->data_seek(0);
 
-$objWriter = new Xlsx($objPHPExcel);
-$objWriter->save('some_excel_file.xlsx');
+while ($category = $categoryResult->fetch_assoc()) {
+    $categoryId = $category['CategoryID'];
+    $categoryName = $category['CategoryName'];
+
+    // Create a sheet for each category
+    $sheet = $spreadsheet->createSheet();
+    $sheet->setTitle($categoryName);
+
+    // Query to retrieve sales data for each category
+    $salesQuery = "
+        SELECT v.vendor_name as Vendor, s.id AS ID, p.name as Product_Name, s.qty AS Quantity, s.price as Price, s.price * s.qty as Total, s.remarks as Remarks, s.date
+        FROM vendor v
+        RIGHT JOIN sales s ON v.id = s.vendor_id
+        RIGHT JOIN products p ON p.id = s.product_id
+        WHERE s.category_id = $categoryId
+        ORDER BY v.vendor_name ASC, MONTH(s.date), p.name ASC, s.id ASC
+    ";
+
+    $salesResult = $conn->query($salesQuery);
+
+    $rowCount = 1;
+    $currentVendor = null;
+    $currentMonth = null;
+
+    while ($row = $salesResult->fetch_assoc()) {
+        $saleMonth = date('F Y', strtotime($row['date']));
+
+        if ($row['Vendor'] != $currentVendor || $saleMonth != $currentMonth) {
+            $currentVendor = $row['Vendor'];
+            $currentMonth = $saleMonth;
+
+            $sheet->mergeCells('A'.$rowCount.':F'.$rowCount);
+            $style = $sheet->getStyle('A');
+            $alignment = $style->getAlignment();
+            $alignment->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+            $sheet->setCellValue('A'.$rowCount, $row['Vendor'] . ' - ' . $saleMonth);
+            $rowCount++;
+
+            // Add headers
+            $sheet->setCellValue('A'.$rowCount, 'Product_Name');
+            $sheet->setCellValue('B'.$rowCount, 'Quantity');
+            $sheet->setCellValue('C'.$rowCount, 'Price');
+            $sheet->setCellValue('D'.$rowCount, 'Total');
+            $sheet->setCellValue('E'.$rowCount, 'Remarks');
+            $sheet->setCellValue('F'.$rowCount, 'Date');
+            $rowCount++;
+        }
+
+        $sheet->setCellValue('A'.$rowCount, $row['Product_Name']);
+        $sheet->setCellValue('B'.$rowCount, $row['Quantity']);
+        $sheet->setCellValue('C'.$rowCount, $row['Price']);
+        $sheet->setCellValue('D'.$rowCount, $row['Total']);
+        $sheet->setCellValue('E'.$rowCount, $row['Remarks']);
+        $sheet->setCellValue('F'.$rowCount, $row['date']);
+        $rowCount++;
+    }
+}
+
+$timestamp = date('Ymd_His');
+$filename = 'sales_' . $timestamp . '.xlsx';
+$objWriter = new Xlsx($spreadsheet);
+$objWriter->save($filename);
+
+header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+header('Content-Disposition: attachment;filename="' . $filename . '"');
+header('Cache-Control: max-age=0');
+
+header('Location: sales.php');
+exit;
 ?>
